ce29b8336ba442e84108fe7ef071ca2dff18297e
Philippe Laban <plaban@microsoft.com>
2024-12-16 18:39:28 -0500

    adding Tuhin's non-fiction silver data

--- gen_all.sh
+++ gen_all.sh
@@ -1,9 +1,12 @@
-python generate_datasets.py --include_pairwise_pref --skip_test
-python generate_datasets.py --include_reward_scoring --skip_test
+python generate_datasets.py --include_pairwise_pref --skip_test # Train: P
+# python generate_datasets.py --include_reward_scoring --skip_test # Train: R
+python generate_datasets.py --include_silver_pairwise --skip_test # Train: S
+python generate_datasets.py --include_pairwise_pref --include_silver_pairwise --skip_test # Train: PS
+
 # python generate_datasets.py --include_gold_pairwise
 # python generate_datasets.py --include_silver_pairwise
-python generate_datasets.py --include_pairwise_pref --include_reward_scoring --skip_test
+# python generate_datasets.py --include_pairwise_pref --include_reward_scoring --skip_test
 # python generate_datasets.py --include_pairwise_pref --include_silver_pairwise
 # python generate_datasets.py --include_reward_scoring --include_silver_pairwise
 # python generate_datasets.py --include_reward_scoring --include_pairwise_pref --include_gold_pairwise --include_silver_pairwise
-python generate_datasets.py --include_reward_scoring --include_pairwise_pref --include_gold_pairwise --include_silver_pairwise --skip_train --include_subedits
+python generate_datasets.py --include_reward_scoring --include_pairwise_pref --include_gold_pairwise --include_silver_pairwise --skip_train --include_subedits # Test: RPSG
